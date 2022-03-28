d4d9317e41f4de8dbdcf128af4d1974f302c47c8
Stergios Papadimitriou <sterg@teikav.edu.gr>
2022-03-28 14:00:16 +0300

    experimenting with the new Java Vector API

--- kotlinLab.sh
+++ kotlinLab.sh
@@ -1,2 +1,2 @@
 
-java -Djava.library.path=.:./lib:. -Dkotlin.compiler.classpath=./lib/*:/libKotlin/*  -XX:+UseNUMA -XX:+UseParallelGC -XX:+UseCompressedOops    -Xss5m -Xms2000m -Xmx25000m -jar kotlinLab.jar
+java -Djava.library.path=.:./lib:. --add-modules=jdk.incubator.vector -Dkotlin.compiler.classpath=./lib/*:/libKotlin/*  -XX:+UseNUMA -XX:+UseParallelGC -XX:+UseCompressedOops    -Xss5m -Xms2000m -Xmx25000m -jar kotlinLab.jar
